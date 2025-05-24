0b4db3309e5d5df3675220c4bd1914b31bf31d41
Orji Michael Chukwuebuka <orjimichael@4886@gmail.com>
2025-05-24 16:54:30 +0100

    Add notes and husky precommit linting

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { TodoModule } from './todo/todo.module';
 
 @Module({
   imports: [TodoModule],
-  controllers: [AppController],
+  controllers: [AppController     ],
   providers: [AppService],
 })
 export class AppModule {}
